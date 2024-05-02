dd09716b7263b09ba3bb6ed3802e91ce6e60ee8d
Rafaeldev2 <116116255+Rafaeldev2@users.noreply.github.com>
2024-05-02 18:53:57 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+        <p className="more_text_1">
+            Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <div className="card">
+        <p className="more_text_2">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+      <div className="card">
+        <p className="more_text_3">
+          welcome home
+        </p>
+      </div>
+      <div className="card">
+        <p className="more_text_4">
+          hello world
+        </p>
+      </div>
+      <div className="card">
+        <p className="more_text_5">
+          hello world
+        </p>
+      </div>
+      <div className="card">
+        <p className="more_text_6">
+          hello world
+        </p>
+      </div>
+      <div className="card">
+        <p className="more_text_7">
+          hello world
+        </p>
+      </div>
+      <div className="card">
+        <p className="more_text_8">
+          hello world
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
