d87ffcc64b933b2cf135e3f7f29b9a17c5316e42
Aluno SENAI <Aluno@SN-358557.edu.fiesc.com.br>
2024-05-03 18:49:32 -0300

    Teste com Cards para Produtos

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,39 @@
+import React, { useState } from 'react';
 import './App.css'
+import ProductCard from './Components/card.jsx';
+
 
 function App() {
+  
+  const [products, setProducts] = useState([]);
+  const addProduct = (newProduct) => {
+    setProducts([...products, newProduct]);
+  };
 
   return (
     <>
-      <div className="card">
-        <p className="more_text_1">
-            Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    <nav className="nav-bar">
+      <li></li>
+    </nav>
+      <div className="app">
+        <h1>Minha Loja de Bijuterias</h1>
+          <div className="product-list">
+            {products.map(Product => (
+              <ProductCard key={Product.id} Product={Product} />
+            ))}
+          </div>
       </div>
-      <div className="card">
-        <p className="more_text_2">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
-      <div className="card">
-        <p className="more_text_3">
-          welcome home
-        </p>
-      </div>
-      <div className="card">
-        <p className="more_text_4">
-          hello world
-        </p>
-      </div>
-      <div className="card">
-        <p className="more_text_5">
-          hello world
-        </p>
-      </div>
-      <div className="card">
-        <p className="more_text_6">
-          hello world
-        </p>
-      </div>
-      <div className="card">
-        <p className="more_text_7">
-          hello world
-        </p>
-      </div>
-      <div className="card">
-        <p className="more_text_8">
-          hello world
-        </p>
-      </div>
+      <button onClick={() => {
+        // Exemplo: Adicionar um novo produto quando o botão for clicado
+        const newProduct = new products(
+          products.length + 1,
+          'Novo Produto',
+          'Descrição do Novo Produto',
+          0, // Preço e imagem podem ser inicializados como 0 ou vazios e atualizados posteriormente
+          ''
+        );
+        addProduct(newProduct);
+      }}>Adicionar Novo Produto</button>
     </>
   )
 }
